e63f8efbf527126cd8c746e8030d4d63f24dc06e
hamza7p_auto <mhamadhamzaalattar.75@gmail.com>
2025-07-05 12:49:12 +0300

    intial project

--- src/App.js
+++ src/App.js
@@ -30,22 +30,23 @@ function App() {
           <Sidebar isSidebar={isSidebar} />
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
-            <Routes>
-              {/* <Route path="/" element={<Dashboard />} /> */}
+            
+            {/* <Routes>
+              {/* <Route path="/" element={<Dashboard />} /> 
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              {/* <Route path="/faq" element={<FAQ />} /> */}
+              {/* <Route path="/faq" element={<FAQ />} /> 
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/school" element={<School />} />
               <Route path="/schedule" element={<Schedule />} />
               <Route path="/students" element={<Students />} />
               <Route path="/year" element={<Year />} />
 
-            </Routes>
+            </Routes> */}
           </main>
         </div>
       </ThemeProvider>
